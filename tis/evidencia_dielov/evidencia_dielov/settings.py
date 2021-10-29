093430484ebce0b1d7271b26502707c3dac8f17a
nikolakulikova <64915683+nikolakulikova@users.noreply.github.com>
2021-10-29 23:16:00 +0200

    Update settings.py

--- tis/evidencia_dielov/evidencia_dielov/settings.py
+++ tis/evidencia_dielov/evidencia_dielov/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q(tn341)1^nq(n_c$%hz4-x5fo6^$5pk@5pdwh!88#))*%3c_l'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
