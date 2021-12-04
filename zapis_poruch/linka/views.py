8e9b2b06ccb0a0c8791e45ae046b414433b1b4e5
nikolakulikova <kulikova.n12345@gmail.com>
2021-12-04 22:09:15 +0100

    pridane naplnanie templatov a rendrovanie templatov, delete tlacitko pri zaznamoch , post pri pridavani

--- zapis_poruch/linka/views.py
+++ zapis_poruch/linka/views.py
@@ -1,6 +1,10 @@
 from django.views.generic import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
+from .forms import TypForm, ZaznamForm
+from .managment.commands.seed import run_seed
+from .models import *
 
 
 # Create your views here.
@@ -8,7 +12,15 @@ class TypyChyb(View):
     template = "chyby_typy.html"
 
     def get(self, request):
-        data = {'list': [[1, 2, 3, 4, 5, 6,7,8],[1, 2, 3, 4, 5, 6,7,8]],}
+        #run_seed("")
+        priemerne_trvanie = dict()
+        for object in TypChyby.objects.all():
+            chyby = Chyba.objects.all().filter(typ_chyby=object)
+            # priemerne_trvanie[object] = sum([x.trvanie])
+
+
+
+        data = {'errors': TypChyby.objects.all()}
         return render(request, self.template, data)
 
     def post(self, request):
@@ -18,7 +30,11 @@ class Zaznamy(View):
     template = "zaznamy.html"
 
     def get(self, request):
-        data = {'list': [[1, 2, 3, 4, 5, 6,3, 4, 5, 6],[1, 2, 3, 4, 5, 6,3, 4, 5, 6]],}
+        if "delete" in request.GET:
+            i = request.GET["id"]
+            chyba = Chyba.objects.all().filter(id=i)
+            chyba.delete()
+        data = {'zaznamy': Chyba.objects.all()}
         return render(request, self.template, data)
 
     def post(self, request):
@@ -28,27 +44,60 @@ class PridajTyp(View):
     template = "pridaj_typ.html"
 
     def get(self, request):
-        data = {'list': [[1, 2, 3, 4, 5, 6],[1, 2, 3, 4, 5, 6]],}
+        data = dict()
+
+        if "id" not in request.GET:
+            data["form"] = TypForm()
+            return render(request, self.template, data)
+
+        i = request.GET["id"]
+        data["form"] = TypForm(instance=TypChyby.objects.all().filter(id=i)[0])
         return render(request, self.template, data)
 
     def post(self, request):
-        return HttpResponse('podarilo sa')
+        if "id" in request.GET:
+            typ = TypChyby.objects.all().filter(id=request.GET["id"])[0]
+            form = TypForm(request.POST, instance=typ)
+        else:
+            form = TypForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+
+        return redirect("typy")
 
 class PridajZaznam(View):
     template = "pridaj_zaznam.html"
 
     def get(self, request):
-        data = {'list': [[1, 2, 3, 4, 5, 6],[1, 2, 3, 4, 5, 6]],}
+        data = dict()
+
+        if "id" not in request.GET:
+            data["form"] = ZaznamForm()
+            return render(request, self.template, data)
+
+        i = request.GET["id"]
+        data["form"] = ZaznamForm(instance=Chyba.objects.all().filter(id=i)[0])
         return render(request, self.template, data)
 
     def post(self, request):
-        return HttpResponse('podarilo sa')
+        if "id" in request.GET:
+            typ = Chyba.objects.all().filter(id=request.GET["id"])[0]
+            form = ZaznamForm(request.POST, instance=typ)
+        else:
+            form = ZaznamForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+        return redirect("zaznamy")
 
 class Revizia(View):
     template = "revizia.html"
 
     def get(self, request):
-        data = {'list': [[1, 2, 3],[1, 2, 3]],}
+        data = {'revizie': TypRevizie.objects.all()}
         return render(request, self.template, data)
 
     def post(self, request):
