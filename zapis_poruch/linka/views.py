352e5611fd0556dd4e9d7c4559c920603b72cde9
nikolakulikova <kulikova.n12345@gmail.com>
2021-12-06 22:03:10 +0100

    pridany view pre pridanie revizie, delete pri reviziach

--- zapis_poruch/linka/views.py
+++ zapis_poruch/linka/views.py
@@ -2,9 +2,10 @@ from django.views.generic import View
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-from .forms import TypForm, ZaznamForm
+from .forms import TypForm, ZaznamForm, RevizieForm
 from .managment.commands.seed import run_seed
 from .models import *
+from datetime import date, timedelta
 
 
 # Create your views here.
@@ -12,19 +13,18 @@ class TypyChyb(View):
     template = "chyby_typy.html"
 
     def get(self, request):
-        #run_seed("")
+        # run_seed("")
         priemerne_trvanie = dict()
         for object in TypChyby.objects.all():
             chyby = Chyba.objects.all().filter(typ_chyby=object)
-            # priemerne_trvanie[object] = sum([x.trvanie])
+            oct = sum([(x.vyriesenie - x.vznik).days for x in chyby]) / len(chyby)
 
-
-
-        data = {'errors': TypChyby.objects.all()}
+        data = {'errors': TypChyby.objects.all(), 'zaznamy': Chyba.objects.all()}
         return render(request, self.template, data)
 
     def post(self, request):
         return HttpResponse('podarilo sa')
+
 
 class Zaznamy(View):
     template = "zaznamy.html"
@@ -39,6 +39,7 @@ class Zaznamy(View):
 
     def post(self, request):
         return HttpResponse('podarilo sa')
+
 
 class PridajTyp(View):
     template = "pridaj_typ.html"
@@ -64,8 +65,8 @@ class PridajTyp(View):
         if form.is_valid():
             form.save()
 
-
         return redirect("typy")
+
 
 class PridajZaznam(View):
     template = "pridaj_zaznam.html"
@@ -93,21 +94,54 @@ class PridajZaznam(View):
 
         return redirect("zaznamy")
 
+
+class PridajRevizia(View):
+    template = "pridaj_revizia.html"
+
+    def get(self, request):
+        data = dict()
+
+        if "id" not in request.GET:
+            data["form"] = RevizieForm()
+            return render(request, self.template, data)
+
+        i = request.GET["id"]
+        data["form"] = RevizieForm(instance=TypRevizie.objects.all().filter(id=i)[0])
+        return render(request, self.template, data)
+
+    def post(self, request):
+        if "id" in request.GET:
+            typ = TypRevizie.objects.all().filter(id=request.GET["id"])[0]
+            form = RevizieForm(request.POST, instance=typ)
+        else:
+            form = RevizieForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+        return redirect("revizia")
+
+
 class Revizia(View):
     template = "revizia.html"
 
     def get(self, request):
-        data = {'revizie': TypRevizie.objects.all()}
+        if "delete" in request.GET:
+            i = request.GET["id"]
+            revizia = TypRevizie.objects.all().filter(id=i)
+            revizia.delete()
+        data = {'revizie': TypRevizie.objects.all(), 'today': date.today(), 'weeks': date.today() + timedelta(days=28)}
         return render(request, self.template, data)
 
     def post(self, request):
         return HttpResponse('podarilo sa')
 
+
 class Grafy(View):
     template = "grafy.html"
 
     def get(self, request):
-        data = {'list': [[1, 2, 3, 4, 5, 6],[1, 2, 3, 4, 5, 6]],}
+        data = {'list': [[1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6]], }
         return render(request, self.template, data)
 
     def post(self, request):
