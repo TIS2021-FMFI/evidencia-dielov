002d3805a1fcd7fa8cb926ee2f45339d49d4c395
nikolakulikova <kulikova.n12345@gmail.com>
2021-12-09 15:15:48 +0100

    url a view pre pouzivatela

--- zapis_poruch/zapis_poruch/urls.py
+++ zapis_poruch/zapis_poruch/urls.py
@@ -4,6 +4,12 @@ from linka.views import TypyChyb, Zaznamy, Grafy, Revizia, PridajTyp, PridajZazn
 
 from linka.views import PridajRevizia
 
+from linka.views import PotvrdZaznam
+
+from linka.views import Pouzivatelia
+
+from linka.views import PridajPouzivatela
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('typy/', TypyChyb.as_view(), name='typy'),
@@ -11,8 +17,11 @@ urlpatterns = [
     path('grafy/', Grafy.as_view(), name='grafy'),
     path('typy/pridaj', PridajTyp.as_view(), name='pridaj_typ'),
     path('zaznamy/pridaj', PridajZaznam.as_view(), name='pridaj_zaznam'),
+    path('zaznamy/potvrd', PotvrdZaznam.as_view(), name='potvrd_zaznam'),
     path('revizia/pridaj', PridajRevizia.as_view(), name='pridaj_zaznam'),
     path('revizia/', Revizia.as_view(), name='revizia'),
+    path('pouzivatelia/', Pouzivatelia.as_view(), name='pouzivatelia'),
+    path('pouzivatelia/pridaj', PridajPouzivatela.as_view(), name='pridaj_pouzivatela'),
 
 
 ]
