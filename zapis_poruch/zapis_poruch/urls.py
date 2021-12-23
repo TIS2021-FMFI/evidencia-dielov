1b9216a8fcb1da676da2c2d18637609bc21d746c
hutnikus <59333824+hutnikus@users.noreply.github.com>
2021-12-23 15:16:09 +0100

    upravena struktura urls

--- zapis_poruch/zapis_poruch/urls.py
+++ zapis_poruch/zapis_poruch/urls.py
@@ -7,21 +7,18 @@ from linka.views import TypyChyb, Zaznamy, Grafy, Revizia, PridajTyp, PridajZazn
 
 
 urlpatterns = [
+    path('login/', Login.as_view(), name='login'),
+    path('logout/', Logout.as_view(), name='logout'),
     path('admin/', admin.site.urls),
     path('', Zaznamy.as_view(), name='typy'),
     path('typy/', TypyChyb.as_view(), name='typy'),
-    path('login/', Login.as_view(), name='login'),
-    path('zaznamy/', Zaznamy.as_view(), name='zaznamy'),
-    path('grafy/', Grafy.as_view(), name='grafy'),
     path('typy/pridaj', PridajTyp.as_view(), name='pridaj_typ'),
+    path('zaznamy/', Zaznamy.as_view(), name='zaznamy'),
     path('zaznamy/pridaj', PridajZaznam.as_view(), name='pridaj_zaznam'),
     path('zaznamy/potvrd', PotvrdZaznam.as_view(), name='potvrd_zaznam'),
-    path('revizia/pridaj', PridajRevizia.as_view(), name='pridaj_zaznam'),
+    path('grafy/', Grafy.as_view(), name='grafy'),
     path('revizia/', Revizia.as_view(), name='revizia'),
+    path('revizia/pridaj', PridajRevizia.as_view(), name='pridaj_zaznam'),
     path('email/', Email.as_view(), name='email'),
-    path('logout/', Logout.as_view(), name='logout'),
-    path('email/', Email.as_view(), name='email'),
-
-
 
 ]
